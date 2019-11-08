5b2c2c4a13586df1a5da823258771649ff9a055b
Hans8309 <hanyangshe@gmail.com>
2019-11-08 12:06:50 +0800

    readme 修改

--- hello.go
+++ hello.go
@@ -4,10 +4,19 @@ import (
 	"fmt"
 )
 
+func variable() {
+	var a, b int = 3, 4
+	var s string = "HYS"
+
+	fmt.Printf("%d  %d  %s\n", a, b, s)
+}
+
 func main() {
 	//注释测试
 	fmt.Println("Hello,世界")
 	fmt.Println("github第一次编辑修改")
-	fmt.Println("本地第18次修改")
-	fmt.Scanln()
+	fmt.Println("本地第19次修改")
+	//fmt.Scanln()
+	variable()
+
 }
